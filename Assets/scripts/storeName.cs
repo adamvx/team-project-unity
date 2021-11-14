970469f16a4893efa3ae4acac5c80becaa60783a
Matthew55661 <58272696+Matthew55661@users.noreply.github.com>
2021-11-14 14:30:21 +0100

    added showing names above head from UI

---
+++ Assets/scripts/storeName.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class storeName : MonoBehaviour
+{
+
+    public static string theName;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void readStringInput( string s )
+    {
+        theName = s;
+        
+    }
+}
+    
