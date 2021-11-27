6a09eb4f6533e2e4c6a64182aa7967d3a2696670
Matthew55661 <58272696+Matthew55661@users.noreply.github.com>
2021-11-27 15:53:38 +0100

    display link

--- Assets/scripts/displayName.cs
+++ Assets/scripts/displayName.cs
@@ -5,20 +5,24 @@ using UnityEngine.UI;
 using TMPro;
 public class displayName : MonoBehaviour
 {
-     public string nameValue;
+    public string nameValue;
+    public string linkValue;
     public TextMeshPro textElement;
     public TextMeshProUGUI UIName;
+    public TextMeshProUGUI UILink;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         nameValue = storeName.theName;
+        linkValue = storeName.theLink;
         textElement.text = nameValue;
         UIName.text = nameValue;
+        UILink.text = linkValue;
     }
 }
--- Assets/scripts/storeName.cs
+++ Assets/scripts/storeName.cs
@@ -6,23 +6,30 @@ public class storeName : MonoBehaviour
 {
 
     public static string theName;
-    
+    public static string theLink;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void readStringInput( string s )
+    public void readStringNameInput(string s)
     {
         theName = s;
-        
+        Debug.Log(theName);
+
+    }
+    public void readStringLinkInput(string s)
+    {
+
+        theLink = s;
+
     }
 }
-    
