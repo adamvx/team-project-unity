56894421bf044a5defc4148c9eadc8549d20f508
Adam Vician <adamvician@gmail.com>
2021-11-27 15:47:21 +0100

    Api edited

--- Assets/scripts/Api.cs
+++ Assets/scripts/Api.cs
@@ -1,15 +1,15 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public static class Api
+public class Api : MonoBehaviour
 {
-  private static readonly string BASE_URL = "http://161.35.216.12:5677/";
-
-  public static IEnumerator CreateRoom(System.Action<Room> callback)
+  private static readonly string HTTP = "http://";
+  private static readonly string WS = "ws://";
+  private static readonly string BASE_URL = "161.35.216.12:5677/";
+  public IEnumerator CreateAndJoinRoom()
   {
-    string uri = BASE_URL + "create";
+    string uri = HTTP + BASE_URL + "create";
 
     UnityWebRequest uwr = UnityWebRequest.Get(uri);
     yield return uwr.SendWebRequest();
@@ -21,7 +21,12 @@ public static class Api
     }
 
     var room = JsonUtility.FromJson<Room>(uwr.downloadHandler.text);
-    callback(room);
+    JoinRoom(room.id);
+  }
 
+  public void JoinRoom(string code)
+  {
+    var finalLink = WS + BASE_URL;
+    // ...
   }
 }
