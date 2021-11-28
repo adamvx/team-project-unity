bf28daf19b18ca96967a7d8f515402647ffe1804
Adam Vician <adamvician@gmail.com>
2021-11-28 14:27:11 +0100

    Draco reimported as raw files

---
+++ Assets/scripts/EncoderTest.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using Draco;
+
+public class EncoderTest : MonoBehaviour
+{
+    async void Start()
+    {
+        // Load file into memory
+        var fullPath = Path.Combine(Application.streamingAssetsPath, "file0.drc");
+        var data = File.ReadAllBytes(fullPath);
+
+        // Convert data to Unity mesh
+        var draco = new DracoMeshLoader();
+        // Async decoding has to start on the main thread and spawns multiple C# jobs.
+        var mesh = await draco.ConvertDracoMeshToUnity(data);
+
+        if (mesh != null)
+        {
+            // Use the resulting mesh
+            GetComponent<MeshFilter>().mesh = mesh;
+        }
+
+        // var result = Draco.Encoder.DracoEncoder.EncodeMesh(gameObject.GetComponent<MeshFilter>().mesh);
+        // Debug.Log(result[0].data);
+    }
+
+}
